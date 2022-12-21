4ab6a1f1856118fdbbc6167a2a24b2e24616b46d
meowyx <qafka256@gmail.com>
2022-12-22 04:11:32 +0600

    added daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,15 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: ['dracula'],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'dracula',
+  },
 }
